8d68eb833b55e28a998ecb533638dc9dfe515655
JensDiemer <git@jensdiemer.de>
2023-03-14 20:17:16 +0100

    Test "update_req"

--- manage_django_project/tests/__init__.py
+++ manage_django_project/tests/__init__.py
@@ -1,4 +1,9 @@
+import unittest.util
 from pathlib import Path
 
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
+
+
+# Hacky way to expand the failed test output:
+unittest.util._MAX_LENGTH = 200
