668a6aa41069ded572ad78d85438c2e86fbe48b4
JensDiemer <git@jensdiemer.de>
2023-03-15 09:30:22 +0100

    test "project_info" command

--- manage_django_project/tests/command_test_utils.py
+++ manage_django_project/tests/command_test_utils.py
@@ -1,3 +1,5 @@
+import inspect
+
 from django.core.management import call_command
 from manageprojects.test_utils.subprocess import SubprocessCallMock
 
@@ -5,6 +7,8 @@ from manage_django_project.tests import PROJECT_ROOT
 
 
 def call_command_capture_subprocess(cmd_module) -> list[str]:
+    assert inspect.ismodule(cmd_module)
+
     with SubprocessCallMock() as call_mock:
         call_command(cmd_module.Command())
 
