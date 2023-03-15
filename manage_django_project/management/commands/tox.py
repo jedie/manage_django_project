6a58ee564ba25b8543e66f547bdd2b1f8330598d
JensDiemer <git@jensdiemer.de>
2023-03-15 10:07:00 +0100

    "tox" command: exit "normal" on failed run

--- manage_django_project/management/commands/tox.py
+++ manage_django_project/management/commands/tox.py
@@ -13,7 +13,7 @@ class Command(BasePassManageCommand):
         super().run_from_argv(argv)
 
         # Just pass every argument to the origin tox CLI:
-        verbose_check_call(sys.executable, '-m', 'tox', *argv[2:])
+        verbose_check_call(sys.executable, '-m', 'tox', *argv[2:], exit_on_error=True)
 
         coverage_combine_report(verbose=True)
 
