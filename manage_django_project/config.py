c4b6eaf28275585b2e175c5299dedf2272438027
JensDiemer <git@jensdiemer.de>
2023-03-16 09:13:55 +0100

    Remove `prod_settings` and add `local_settings_commands` to `ManageConfig`
    The idea is: `manage_django_project` should never been used on prod (for now)

--- manage_django_project/config.py
+++ manage_django_project/config.py
@@ -6,19 +6,11 @@ from typing import Optional
 from bx_py_utils.path import assert_is_file
 from packaging.version import Version
 
-from manage_django_project.management import commands
-
 
 try:
     import tomllib  # New in Python 3.11
 except ImportError:
     import tomli as tomllib
-
-
-def get_own_commands():
-    commands_path = Path(commands.__file__).parent
-    own_commands = [item.stem for item in commands_path.glob('*.py') if not item.stem.startswith('_')]
-    return own_commands
 
 
 @dataclasses.dataclass
@@ -27,9 +19,10 @@ class ManageConfig:
     project_root_path: Path
 
     # dotted name of Django settings for...
-    prod_settings: str  # ... production
     local_settings: str  # ... run local Django dev. server
     test_settings: str  # ... run tests
+
+    local_settings_commands: tuple = ('test', 'coverage', 'tox')
 
 
 @dataclasses.dataclass
@@ -61,15 +54,10 @@ class ProjectInfo:
         return pyproject_toml
 
     def get_settings_by_command(self, *, command_name) -> str:
-        own_commands = get_own_commands()
-
-        if command_name in ('test', 'coverage', 'tox'):
+        if command_name in self.config.local_settings_commands:
             return self.config.test_settings
 
-        if command_name in own_commands:
-            return self.config.local_settings
-
-        return self.config.prod_settings
+        return self.config.local_settings
 
     def get_current_settings(self, argv) -> Optional[str]:
         for arg in argv:
@@ -77,13 +65,10 @@ class ProjectInfo:
                 # e.g.: Start a manage command with --settings option -> don't force any settings
                 return
 
-        settings_name = self.config.prod_settings
-
-        if len(argv) == 1:
-            # e.g.: just start "./manage.py"
-            settings_name = self.config.local_settings
-        elif len(argv) > 1:
+        if len(argv) > 1:
             settings_name = self.get_settings_by_command(command_name=argv[1])
+        else:
+            settings_name = self.config.local_settings
 
         return settings_name
 
