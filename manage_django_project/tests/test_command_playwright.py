435b0006fe2d50509513ac5c26f0909cebd19f28
Jens Diemer <git@jensdiemer.de>
2025-07-29 20:10:34 +0200

    Add CallManagePy test helper (#68)

--- manage_django_project/tests/test_command_playwright.py
+++ manage_django_project/tests/test_command_playwright.py
@@ -1,15 +1,16 @@
 from cli_base.cli_tools.test_utils.rich_test_utils import NoColorEnvRichClick
 from django.test import SimpleTestCase
 
-from manage_django_project.tests.test_manage import call_manage_py
+from manage_django_project.test_utilities import CallManagePy
 
 
 class TestCase(SimpleTestCase):
     maxDiff = None
 
     def test_happy_path(self):
+        manage_py = CallManagePy()
         with NoColorEnvRichClick(width=80):
-            output = call_manage_py('playwright', '--help')
+            output = manage_py.verbose_check_output('playwright', '--help')
 
         self.assertIn('manage_django_project_example playwright --help\n', output)
         self.assertIn('Usage: playwright [options] [command]\n', output)
