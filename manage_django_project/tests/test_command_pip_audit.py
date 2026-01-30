f0bd6b8ff8e4e3f5fa6bbf54b186bc127bc62caf
JensDiemer <git@jensdiemer.de>
2026-01-30 20:49:34 +0100

    Bugfix tests around "pip-audit"

--- manage_django_project/tests/test_command_pip_audit.py
+++ manage_django_project/tests/test_command_pip_audit.py
@@ -1,21 +1,14 @@
 from unittest.mock import patch
 
+from bx_py_utils.test_utils.redirect import RedirectOut
+from cli_base.cli_tools.test_utils.temp_utils import FakeNamedTemporaryFile
+from django.core.management import call_command
 from django.test import SimpleTestCase
+from manageprojects.test_utils.subprocess import SimpleRunReturnCallback, SubprocessCallMock
 
 from manage_django_project.management.commands import pip_audit, shell
 from manage_django_project.tests.cmd2_test_utils import BaseShellTestCase
-from manage_django_project.tests.command_test_utils import call_command_capture_subprocess
-
-
-class FakeNamedTemporaryFile:
-    def __init__(self, prefix: str = 'prefix', suffix: str = 'suffix'):
-        self.name = f'/tmp/{prefix}<rnd>{suffix}'
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+from manage_django_project.tests.command_test_utils import get_rstrip_paths
 
 
 class PipAuditShellTestCase(BaseShellTestCase):
@@ -31,8 +24,19 @@ class PipAuditTestCase(SimpleTestCase):
     maxDiff = None
 
     def test_pip_audit(self):
-        with patch('tempfile.NamedTemporaryFile', FakeNamedTemporaryFile):
-            popenargs = call_command_capture_subprocess(cmd_module=pip_audit)
+        with (
+            RedirectOut() as buffer,
+            SubprocessCallMock(
+                return_callback=SimpleRunReturnCallback(stdout=b'mocked output'),  # type: ignore
+            ) as call_mock,
+            patch('tempfile.NamedTemporaryFile', FakeNamedTemporaryFile),
+        ):
+            call_command(pip_audit.Command())
+
+        self.assertEqual(buffer.stderr, '')
+        self.assertIn('Run `pip-audit` with configuration from `pyproject', buffer.stdout)
+
+        popenargs = call_mock.get_popenargs(rstrip_paths=get_rstrip_paths())
         self.assertEqual(
             popenargs,
             [
@@ -43,8 +47,6 @@ class PipAuditTestCase(SimpleTestCase):
                     '--frozen',
                     '--no-editable',
                     '--no-emit-project',
-                    '-o',
-                    '/tmp/requirements<rnd>.txt',
                 ],
                 [
                     '.../bin/pip-audit',
